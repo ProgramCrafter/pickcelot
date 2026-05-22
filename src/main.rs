230da9965086aee3ba94418bc3e664c5eb9e74b7
Ender Ting <pythonhunter146@gmail.com>
2026-05-22 09:13:14 +0300

    Fractal (not) drawing

--- src/main.rs
+++ src/main.rs
@@ -67,11 +67,14 @@ local term = {
         gpu.setForeground(0xFFFFFF)
     end
 }
+local ost = 0.0
 local os = {
     sleep = function(t)
+        if t <= 0.0 then return piccolo_yield() end
+        ost = ost + t
         return piccolo_component_invoke('$os', 'os.sleep', t)
     end,
-    time = function() return 1.0 end
+    time = function() ost = ost + 1.0 return ost end
 }
 local hologram
 hologram = {
@@ -109,137 +112,199 @@ hologram = {
     end,
     clear = term.clear
 }
+local computer = {}
 
 
-local c = 20
---моделируем Снеговика
-local tSnowman = {10,10,11,11,11,12,12,12,12,11,11,11,10,9,8,7,6,7,8,8,9,9,9,9,9,8,8,7,6,7,6,6,6,6,6,6}
---таблица падающих снежинок(Взаимствована у Doob, надеюсь он не против)
-local tSnow = {}
-for sn = 1, 32 do
-  tSnow[sn] = {}
+local function pullSignal(t)
+    os.sleep(t or 0.0)
 end
---палитра цветов
-hologram.setPaletteColor(1, 0xFFFFFF)--белый
-hologram.setPaletteColor(2, 0x8B0000)--бардовый
-hologram.setPaletteColor(3, 0xFFA500)--оранжевый
 
---цикл рисования круга так же взаимствован у Doob
-local function circle(x0, y, z0, R, i)
-  local x = R
-  local z = 0
-  local err = -R
-  while z <= x do
-    hologram.set(x + x0, y, z+z0, i)
-    hologram.set(z + x0, y, x + z0, i)
-    hologram.set(-x + x0, y, z + z0, i)
-    hologram.set(-z + x0, y, x + z0, i)
-    hologram.set(-x + x0, y, -z + z0, i)
-    hologram.set(-z + x0, y, -x + z0, i)
-    hologram.set(x + x0, y, -z + z0, i)
-    hologram.set(z + x0, y, -x + z0, i)
-    z = z+1
-    if err <= 0 then
-      err = err + (2 * z + 1)
+local xor = function(u,v) return u ~ v end
+local abs=math.abs
+local cos=math.cos
+local sin=math.sin
+local char=function(c) return c > 32 and '.' or ' ' end
+local time, this_time = os.time, 0
+local center,max_x,max_y = 0, gpu.getResolution()
+local ys,xs = max_y*4, max_x*2
+local center_x=xs//2-2
+local center_y = ys//2-2
+
+local radius = ys//2-10
+local x,y=0,0
+--опишем более быстрое однобитное ксорирование
+local reverse={} reverse[0]=1 reverse[1]=0
+local chars,screen={},{}
+local ch_y,ch_x = (max_y//1),(max_x//1)
+local xr,yr=0,0
+local mode='0'
+actions={}
+events = {key_up='keyUp'}
+--chars init
+for y=1,ch_y do
+	chars[y]={}
+end
+--screen init
+for y=1.0,ys do
+	screen[y]={}
+end
+--перехват ивентов. надстройка над ОС
+function computer.pullSignal(...)
+    local e = {pullSignal(...)}
+       if events[e[1]] then
+           return actions[events[e[1]]](e)
+       end
+   return true --table.unpack(e) --true --table.unpack(e) 
+end
+-----------------------------------
+actions.t=function()
+    --tetminate
+    mode='terminate'
+    term.clear()
+    screen=nil
+    chars=nil
+    computer.pullSignal = pullSignal
+    evo=nil
+    return os.exit()
+end
+actions['1']=function()
+ mode='0'
+ return true
+end
+actions['2']=function()
+mode = '1'
+return true
+end
+---------------------------------
+actions.keyUp=function(e)
+    local key=math.floor(e[3])
+    if key > 128 then
+        key = string.lower(ru_keys[key])
     else
-      x = x - 1
-      err = err + (2 * (z - x) + 1)
+        key=string.lower(string.char(key))
     end
-  end
-end
---рисуем пуговицы они же глаза
-local function stud(x, y0, z0, i)
-  for y = 0, 1 do
-    for z = 0, 1 do
-      hologram.set(x, y0-y, z0-z, i)
+    if actions[key] then
+        return actions[key](e)
     end
-  end
-end
--- рисуем снеговика
-local function snowman()
-  for i = 1, 30 do
-    circle(c,i,c, tSnowman[i], 1)--отрисовываем основу снеговика
-  end
-  for i = 30, #tSnowman do
-    circle(c,i,c, tSnowman[i], 2)--рисуем шапку
-  end
-  --рисуем глаза
-  stud(29, 23, 23, 3)
-  stud(29, 23, 17, 3)
-  --рисуем морковку
-  for x = 1, 6  do
-    for y = 1, 2 do
-      for z = 1, 2 do
-        hologram.set(27+x, 18+z, 18+y, 3)
-      end
-    end
-  end
-  -- правая рука
-  for x = 1, 7 do
-    y = 3
-    hologram.set(20+y, 10+x, 10-x, 3)
-  end
-  -- левая рука
-  for x = 1, 7 do
-    y = 1
-    for y = 1, 3 do
-      y = y
-    end
-    hologram.set(20-y, 10+x, 31+x, 3)
-  end
-  --рисуем пуговицы
-  stud(30, 14, 20, 2)
-  stud(32, 10, 20, 2)
-  stud(32, 6, 20, 2)
-
+    return true
 end
 
-local function gen_snow() -- генерируем таблицу для источника снежинок(из кода Doob)
-  local tbl = {}
-  local x, y
-  for g = 1, 10 do
-    x, y = math.random(1, 46), math.random(1, 46)
-    if tbl[x] == nil then
-      tbl[x] = {}
-    end
-    tbl[x][y] = 1
-  end
-  return tbl
-end
 
-local function falling_snow() -- слвигаем таблицы со снежинками вниз(Doob)
-  tmp = {}
-  for s = 1, 32 do
-    tmp[s] = {}
-    tmp[s] = tSnow[s+1]
-  end
-  tSnow = tmp
-  tSnow[32] = gen_snow()
-end
- 
-local function animation(de) -- задаем указанное значение для снежинок(Doob)
-  for x = 1, 46 do
-    for z = 1, 46 do
-      for y = 1, 32 do
-        if tSnow[y][z] ~= nil then
-          if tSnow[y][z][x] == 1 then
-            hologram.set(x, y, z, de)
-          end
+local function cls_chr()
+    for y = 1,ch_y do 
+        for x = 1,ch_x do 
+            chars[y][x] = 0x2800
         end
-      end
+    end 
+end
+
+local function cls_scr()    
+	for y=1.0,ys do
+		for x=1.0,xs do
+			screen[y][x]=0
+		end
+	end
+end
+
+--опишем биткарту шрифта брайля
+local bits = {} 
+	bits[1]={1,8,2,16,4,32,64,128}
+	bits[0]={0,0,0,0,0,0,0,0}
+	bits[-1]={-1,-8,-2,-16,-4,-32,-64,-128}
+--попробуем описать трансформацию значений массива в шрифт брайля
+local function toUnicode()
+  local ch_x,ch_y,yy,xx=0,0,0,0
+    for y in pairs(screen) do
+        ch_y=y+3  yy=y-1
+        ch_y=math.floor(ch_y/4)
+        for x in pairs(screen[y]) do
+          ch_x=x+1  xx=x-1
+            ch_x=math.floor(ch_x/2)
+            chars[ch_y][ch_x]=chars[ch_y][ch_x]+bits[screen[y][x]][1+(yy%4)*2+xx%2]
+        end
     end
-  end
+    return true
 end
 
-hologram.clear()
-
-while 1 do
-  snowman()
-  falling_snow()
-  animation(1)
-  os.sleep(2)
-  animation(0)
+--отобразим содержимое экрана
+local function showMustGoOne()
+    for y in pairs(chars)do
+        show=''
+        
+        for x in pairs(chars[y])do
+            show = show..char(chars[y][x])
+        end
+        gpu.set(1,y+1,show)
+    end
+    return true
 end
+
+local function pseudo_draw(x1,y1,x,y)--вычисляет координаты точек линии
+	if x < x1 then x_step = -1 else x_step = 1 end
+	if y < y1 then y_step = -1 else y_step = 1 end
+
+	if abs(x-x1) > abs(y-y1) then
+		y_step=y_step*abs(y-y1)/abs(x-x1)
+		y_plot=y1
+		for x_plot = x1,x,x_step do
+			screen[center_y+y_plot//1+1][center_x+x_plot//1+1]=reverse[screen[center_y+y_plot//1+1][center_x+x_plot//1+1]]
+			y_plot=y_plot+y_step
+		end
+	else
+		x_step=x_step*abs(x-x1)/abs(y-y1)
+		x_plot=x1
+		for y_plot = y1,y,y_step do
+			screen[center_y+y_plot//1+1][center_x+x_plot//1+1]=reverse[screen[center_y+y_plot//1+1][center_x+x_plot//1+1]]
+			x_plot=x_plot+x_step
+		end
+	end
+end
+
+cls_scr() cls_chr()
+local x,y,a,angle,step,f,lines=0,0,0,0,17,2,0
+
+function main()
+	gpu.set(1,1,'Press key: (1) - mode 1, (2) - mode 2, (T) - for exit')
+while math.huge do
+	lines=512.0
+	while lines <= 888 do
+		this_time=time()/1000
+		if mode == '1' then 
+			xr=(this_time%(radius-radius/8))*cos(1.0/sin(this_time))--radius/2-4
+			yr=(this_time%(radius-radius/8))*sin(1.0/cos(this_time))--radius/2-4
+		else
+			xr=0
+			yr=0
+		end
+		angle=f*math.pi/lines
+		a= 0
+		for i = 1, lines do
+			if mode == '1' then 
+				xr=(this_time%(radius-radius/8))*cos(1.0/sin(this_time))
+				yr=(this_time%(radius-radius/8))*sin(1.0/cos(this_time))
+			else
+				xr=0
+				yr=0
+			end
+			x=radius*cos(a)
+			y=radius*sin(a)
+			--pseudo_draw(center_x/2,center_y/2,x,y)
+			pseudo_draw(xr,yr,x,y)
+			a=a+angle
+		end
+
+		toUnicode()
+		showMustGoOne()
+		cls_scr()
+		cls_chr()
+		os.sleep((8001-lines)/40000)
+		os.sleep(0.3)
+		lines=lines+step
+	end
+	if step <60 then step=step+3 else step=33 end
+end
+end
+main()
 ".as_bytes();
 
 
@@ -504,7 +569,7 @@ impl eframe::App for App {
             self.peripheral.sleep = f64::NEG_INFINITY;
             
             let ex = ctx.fetch(&self.engine);
-            let mut fuel = Fuel::with(4096);
+            let mut fuel = Fuel::with(40960);
             while ex.step(ctx, &mut fuel) {
                 let request = ex
                     .take_result::<UserData>(ctx)
